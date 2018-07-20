fb18f41452da5cd00cf35ac97018a81f5bf718a1
caofei <1594717129@qq.com>
2018-07-20 09:07:40 +0800

    Initial commit

---
+++ Example/ADMobGenYD/ADMobGenAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADMobGenAppDelegate.h
+//  ADMobGenYD
+//
+//  Created by 1594717129@qq.com on 07/20/2018.
+//  Copyright (c) 2018 1594717129@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ADMobGenAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ADMobGenYD/ADMobGenViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADMobGenViewController.h
+//  ADMobGenYD
+//
+//  Created by 1594717129@qq.com on 07/20/2018.
+//  Copyright (c) 2018 1594717129@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ADMobGenViewController : UIViewController
+
+@end
